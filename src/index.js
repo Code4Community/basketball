6ed9f1347f8c03cf20c5eea10d4482c88b355498
John Doe <iaustin0b@gmail.com>
2023-10-16 19:50:13 -0400

    upd

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const theme = {
 
 C4C.Editor.create(document.body, null, true);
 
-var config = {
+var config = { 
   parent: "body",
   type: Phaser.AUTO,
   width: 800,
--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import Physics from "phaser";
 import logoImg from "../assets/logo.png";
 import smileyImg from "../assets/smiley.png";
+import courtImg from "../assets/C4CbbBKG_upscaled.png"
 import C4C from "c4c-lib";
 
 function enterButtonHoverState(btn) {
@@ -20,73 +21,37 @@ export default class ExampleScene extends Phaser.Scene {
   preload() {
     this.load.image("logo", logoImg);
     this.load.image("smiley", smileyImg);
+    this.load.image("court", courtImg)
   }
 
   create() {
-    // This is the text for a program which calls the function stored in the
-    // symbol "alert-hello". It starts with a line comment, and then a function
-    // call.
-    C4C.Editor.Window.init(this);
-    C4C.Editor.Window.open();
-    C4C.Editor.setText(`moveRight(20)`);
 
-    this.smiley = this.add.sprite(400, 300, "smiley");
+    this.courtImg = this.add.sprite(0,0,"court");
 
-    // Define new function and store it in the symbol "alert-hello". This
-    // function can now be called from our little language.
-    C4C.Interpreter.define("alertHello", () => {
-      alert("hello");
-    });
+}
 
-    C4C.Interpreter.define("moveRight", (x_dist) => {
-      this.smiley.x += x_dist;
-    });
+class CourtBoundaries {
 
-    C4C.Interpreter.define("moveLeft", (x_dist) => {
-      this.smiley.x -= x_dist;
-    });
+    title_increment;
+    full_X_border;
+    full_Y_border;
+    court_offset_X;
+    court_offset_Y;
+    court_border_X;
+    court_border_Y;
 
-    // Create some interface to running the interpreter:
+    constructor(posX, posY) { //posX posY added for unrestricted placement of court
+        this.title_increment = 49;
+        this.full_X_border = 1262 + posX;
+        this.full_Y_border = 346 + posY;
+        this.court_offset_X = 175;
+        this.court_offset_Y = 90;
+        this.court_border_X = this.full_X_border - 2 * this.court_offset_X;
+        this.court_border_Y = this.full_Y_border - 2 * this.court_offset_Y;
+    }
 
-    // Run Button
-    const runButton = this.add
-          .text(550, 100, "Evaluate", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            const programText = C4C.Editor.getText();
-            // HERE'S THE IMPORTANT PART!!
-            C4C.Interpreter.run(programText);
-          })
-          .on("pointerover", () => enterButtonHoverState(runButton))
-          .on("pointerout", () => enterButtonRestState(runButton));
-
-    // Editor Button
-    const editorButton = this.add
-          .text(500, 200, "Toggle Editor", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            C4C.Editor.Window.toggle();
-          })
-          .on("pointerover", () => enterButtonHoverState(editorButton))
-          .on("pointerout", () => enterButtonRestState(editorButton));
-
-    // Check Button
-    const checkButton = this.add
-          .text(550, 300, "Check", { fill: "#fff", fontSize: "30px" })
-          .setInteractive()
-          .on("pointerdown", () => {
-            const programText = C4C.Editor.getText();
-            // HERE'S THE IMPORTANT PART!!
-            
-            try {
-              C4C.Interpreter.check(programText);              
-            } catch (err) {
-              console.log("Caught something: " + err);
-            } finally {
-              console.log("Done handling");
-            }
-          })
-          .on("pointerover", () => enterButtonHoverState(checkButton))
-          .on("pointerout", () => enterButtonRestState(checkButton));
-  }
+    checkCollision(posX, posY){
+      let flag = false;
+      return 
+    }
 }
