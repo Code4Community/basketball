5f35c5900627a8ec4d4ce5581bb366895ca1cc53
Carter Scheatzle <Carter Scheatzle>
2023-10-23 20:26:13 -0400

    Moving game window to top of page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import C4C from "c4c-lib";
 import ExampleScene from "./scenes/ExampleScene";
+import Scene1 from "./scenes/Scene1";
 
 // You can define a custom theme here and pass it into .create below
 const theme = {
@@ -10,7 +11,7 @@ const theme = {
   },
   ".cm-content, .cm-gutter": {
     minHeight: "600px",
-  }
+  },
 }
 
 C4C.Editor.create(document.body, null, true);
@@ -20,6 +21,13 @@ var config = {
   type: Phaser.AUTO,
   width: 1262,
   height: 346,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 0 },
+        debug: false
+    }
+  },
   dom: {
     createContainer: true,
   },
