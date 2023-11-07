554a5d19dd67e5e4d635400cc953674c82b78d0c
adefasola <fasola.5@buckeyemail.osu.edu>
2023-11-06 19:12:27 -0500

    ade

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -4,8 +4,13 @@ import logoImg from "../assets/logo.png";
 import smileyImg from "../assets/smiley.png";
 import court from "../assets/court.png";
 import derrick from "../assets/derrick_rose.jpg";
+import ballz from "../assets/Basketball.png";
 import C4C from "c4c-lib";
 
+
+var ball;
+var player;
+var cursors;
 
 function enterButtonHoverState(btn) {
   btn.setStyle({ fill: "#ff0" });
@@ -25,13 +30,39 @@ export default class Scene1 extends Phaser.Scene {
     this.load.image("smiley", smileyImg);
     this.load.image('court', court);
     this.load.image('playa', derrick);
+    this.load.image('ballz', ball);
   }
 
   create() {
 
     this.add.image(0, 0, 'court').setOrigin(0,0);
-    var player = this.physics.add.sprite(0, 0, 'playa').setOrigin(0,0);
+    player = this.physics.add.sprite(1, 10, 'playa').setOrigin(0,0);
     player.setScale(0.1);
 
+    ball = this.physics.add.sprite(500, 10, 'ballz').setOrigin(0,0);
+    ball.setScale(.1);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
   }
+
+  update() {
+
+    if (cursors.isDown(Phaser.KeyCode.SPACEBAR)) {
+
+      passBall();
+
+    }
+
+  }
+  
+  passBall(){
+
+    this.physics.moveToObject(ball, player);
+  
+  }
+
+  
 }
+
+
