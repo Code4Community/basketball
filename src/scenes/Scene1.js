ab7e00f01b345ecfcc84c3d7fb337f0ce5e30bbf
Carter Scheatzle <Carter Scheatzle>
2023-11-13 19:54:32 -0500

    Getting derrick rose to move.

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -6,6 +6,8 @@ import court from "../assets/court.png";
 import derrick from "../assets/derrick_rose.jpg";
 import C4C from "c4c-lib";
 
+let deltaX = 100;
+let deltaY = 100;
 
 function enterButtonHoverState(btn) {
   btn.setStyle({ fill: "#ff0" });
@@ -24,14 +26,20 @@ export default class Scene1 extends Phaser.Scene {
     this.load.image("logo", logoImg);
     this.load.image("smiley", smileyImg);
     this.load.image('court', court);
-    this.load.image('playa', derrick);
+    this.load.image('derrick', derrick);
   }
 
   create() {
-
     this.add.image(0, 0, 'court').setOrigin(0,0);
-    var player = this.physics.add.sprite(0, 0, 'playa').setOrigin(0,0);
-    player.setScale(0.1);
+    var player = this.physics.add.sprite(0, 50, 'derrick').setOrigin(0,0);
+    player.setScale(0.05);
+
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.keySpace.on('down', (key, event) =>
+    {
+        this.physics.moveTo(player, player.x + deltaX, player.y - deltaY, 200);
+    });
 
   }
 }
